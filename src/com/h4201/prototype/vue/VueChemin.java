80c19bae3cb81cad9c5b1a003a2cfe8b09666f5c
Eva <eva.totoum-nemkam@insa-lyon.fr>
2013-11-16 21:45:08 +0100

    Package vue src

---
+++ src/com/h4201/prototype/vue/VueChemin.java
@@ -0,0 +1,42 @@
+package com.h4201.prototype.vue;
+
+import java.awt.Color;
+
+import com.h4201.prototype.modele.Chemin;
+
+public class VueChemin
+{
+	private Chemin chemin;
+	private boolean etat;
+	
+	public VueChemin()
+	{
+		
+	}
+	
+	public VueChemin(Chemin chemin, Color couleur)
+	{
+		
+	}
+	
+	public Chemin getChemin()
+	{
+		return chemin;
+	}
+	
+	public boolean getEtat()
+	{
+		return etat;		
+	}
+	
+	public void afficherChemin()
+	{
+		
+	}
+	
+	public void desactiverChemin(boolean etat)
+	{
+		
+	}
+	
+}
