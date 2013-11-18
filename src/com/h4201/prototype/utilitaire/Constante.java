a24310854d1ddadbd40e4643bfca39c07c826906
Paul <paul.molins@gmail.com>
2013-11-18 09:46:22 +0100

    Ajout de la conversion mètres en pixels

--- src/com/h4201/prototype/utilitaire/Constante.java
+++ src/com/h4201/prototype/utilitaire/Constante.java
@@ -1,21 +1,22 @@
 package com.h4201.prototype.utilitaire;
 
+/**
+ * 
+ * @author Paul
+ *
+ */
 public abstract class Constante
-{
-	/**
-	 * Etat d'une livraison
-	 */
-//	public static final int NON_LIVRE = 1; // Pt de livraison non livre
-//	public static final int LIVRE = 2; // Pt de livraison livre
-//	
-//	public enum EtatLivraison {
-//		NON_LIVRE, LIVRE
-//	}
-	
-	/**
-	 * Exceptions
+{	
+	/*
+	 * Exception
 	 */
 	public static final String EXCEPTION_XML = "Le XML ne correspond pas à ce qui est attendu";
 	public static final String EXCEPTION_FICHIER = "Le fichier n'existe pas ou est illisible";
 	public static final String EXCEPTION_NON_INSTANCIE = "L'objet n'a pas été instancié";
+	
+	
+	/*
+	 * Conversion
+	 */
+	public static final double CONVERSION_METRES_EN_PIXELS = 0.1;
 }
