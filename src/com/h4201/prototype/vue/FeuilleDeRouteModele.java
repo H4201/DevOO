1bf41f2d2e48722fbd612e24293eea5d48e8a67f
Petitsucre <z.xinlu@gmail.com>
2013-11-18 01:45:28 +0100

    VueFeuilleDeRoute done

---
+++ src/com/h4201/prototype/vue/FeuilleDeRouteModele.java
@@ -0,0 +1,61 @@
+package com.h4201.prototype.vue;
+
+import com.h4201.prototype.modele.PointLivraison;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+
+@SuppressWarnings("serial")
+public class FeuilleDeRouteModele extends AbstractTableModel  {
+
+
+	private final ArrayList<PointLivraison> lesPointsLivraisons = new ArrayList<PointLivraison> ();
+	 
+    private final String[] entetes = {"Point de Livraison", "Adresse de livraison", "Heure d'arrivée", "Heure de départ", "Itinéraire", "Contact Client"};
+    
+
+	public FeuilleDeRouteModele() {
+		 super();
+	      lesPointsLivraisons.add(new PointLivraison(null, null, null)); // ajouter les paramètres
+		
+	}
+
+	@Override
+	public int getColumnCount() {
+		return entetes.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return lesPointsLivraisons.size();
+	}
+	
+	public String getColumnName(int columnIndex) {
+        return entetes[columnIndex];
+    }
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+
+		
+		        switch(columnIndex){
+		            case 0:
+		                return lesPointsLivraisons.get(rowIndex).getIdPointLivraison();
+		            case 1:
+		                return lesPointsLivraisons.get(rowIndex).getNoeud();// ajouter getadresse, adresse n'apparait null part
+		            case 2:
+		                return lesPointsLivraisons.get(rowIndex).getTrancheHoraire().getHeureDebut();
+		            case 3:
+		                return lesPointsLivraisons.get(rowIndex).getTrancheHoraire().getHeureFin();
+		            case 4:
+		                return lesPointsLivraisons.get(rowIndex).getCheminSortant();
+
+		            default:
+		                return null; //Ne devrait jamais arriver
+		        }
+	
+	}
+
+}
--- src/com/h4201/prototype/vue/VueFeuilleDeRoute.java
+++ src/com/h4201/prototype/vue/VueFeuilleDeRoute.java
@@ -1,37 +1,50 @@
 package com.h4201.prototype.vue;
 
-import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-import com.h4201.prototype.modele.Noeud;
-import com.h4201.prototype.modele.PointLivraison;
-import com.h4201.prototype.modele.Tournee;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
 
-public class VueFeuilleDeRoute extends VueSupervision
-{
-	private Tournee tournee;
 
-	public Tournee getTournee()
-	{
-		return tournee;
+@SuppressWarnings("serial")
+public class VueFeuilleDeRoute extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VueFeuilleDeRoute frame = new VueFeuilleDeRoute();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
-	public ArrayList<PointLivraison> getPointDeLivraison(Tournee tournee)
-	{
-		return null;
-	}
 
-	@Override
-	public Noeud clic(double x, double y)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void afficher()
-	{
-		// TODO Auto-generated method stub
+	/**
+	 * Create the frame.
+	 */
+	public VueFeuilleDeRoute() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 		
+		table = new JTable();
+		table.setModel(new FeuilleDeRouteModele());
+		contentPane.add(table, BorderLayout.CENTER);
 	}
 
 }
