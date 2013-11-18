4edde7ab3e15687cb1eafad3b79e1a108decc9b6
Eva <eva.totoum-nemkam@insa-lyon.fr>
2013-11-18 11:56:05 +0100

    g

--- src/com/h4201/prototype/utilitaire/Constante.java
+++ src/com/h4201/prototype/utilitaire/Constante.java
@@ -1,7 +1,5 @@
 package com.h4201.prototype.utilitaire;
 
-import java.awt.Color;
-
 public abstract class Constante
 {
 	/**
@@ -14,18 +12,10 @@ public abstract class Constante
 //		NON_LIVRE, LIVRE
 //	}
 	
-	/*
-	 * constante pour le plan
-	 */
-	public static final int LONGUEUR = 240;
-	public static final int LARGEUR = 240;
-	public static final Color ARRIEREPLAN = Color.BLUE;
-	
 	/**
 	 * Exceptions
 	 */
 	public static final String EXCEPTION_XML = "Le XML ne correspond pas à ce qui est attendu";
 	public static final String EXCEPTION_FICHIER = "Le fichier n'existe pas ou est illisible";
 	public static final String EXCEPTION_NON_INSTANCIE = "L'objet n'a pas été instancié";
-	
 }
