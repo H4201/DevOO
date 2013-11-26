b880bd9b62faf2482c3b4b47fc53db0443c1c1b5
marinajulien <marinajulien@acces0290.res.insa-lyon.fr>
2013-11-27 00:08:13 +0100

    suppression xinlu

--- src/com/h4201/prototype/vue/VueTrancheHoraire.java
+++ src/com/h4201/prototype/vue/VueTrancheHoraire.java
@@ -8,10 +8,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
@@ -19,20 +24,24 @@ import javax.swing.JComboBox;
 import com.h4201.prototype.controleur.Controleur;
 import com.h4201.prototype.modele.Noeud;
 import com.h4201.prototype.modele.PointLivraison;
-
+import com.h4201.prototype.modele.Tournee;
 import com.h4201.prototype.modele.TrancheHoraire;
+import com.h4201.prototype.utilitaire.Constante;
 
 
-public class VueTrancheHoraire extends JFrame {
+public class VueTrancheHoraire extends MouseAdapter {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JComboBox<TrancheHoraire> comboBoxTrancheHoraire=new JComboBox<TrancheHoraire>();
-	private JComboBox<PointLivraison> comboBoxPointLivraison=new JComboBox<PointLivraison>();
-	private static Noeud noeud;
-
-
- private JButton bouton = new JButton("OK");
+	//private JPanel contentPane;
+	private JComboBox<TrancheHoraire> comboBox;
+	private JComboBox<PointLivraison> comboBox2;
+	private JLabel label;
+	//la fenetre
+	private JFrame fenetre;
+	private JButton boutonOK;
+	private TrancheHoraire trancheHoraire;
+	private PointLivraison pointLivraison;
+	private Noeud noeud;
 
 	private static volatile VueTrancheHoraire instance = null;
 
@@ -47,99 +56,155 @@ public class VueTrancheHoraire extends JFrame {
 			{
 				if (VueTrancheHoraire.instance == null)
 				{
-					
-					VueTrancheHoraire.instance = new VueTrancheHoraire( noeud);
+					VueTrancheHoraire.instance = new VueTrancheHoraire();
 				}
 			}
 		}
 
 		return VueTrancheHoraire.instance;
 	}
-	public VueTrancheHoraire(Noeud noeudclique) { 
-	
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		setVisible(true);
-		//contentPane.add(comboBox, BorderLayout.CENTER);
-		contentPane.add(bouton,BorderLayout.SOUTH);
+	private VueTrancheHoraire() {
 		
-		JPanel centre = new JPanel();
-		centre.add(comboBoxTrancheHoraire, BorderLayout.CENTER);
-		contentPane.add(centre,BorderLayout.CENTER);
-		
-		centre.add(comboBoxPointLivraison, BorderLayout.CENTER);
-
-	
-		// bouton.addActionListener(new BoutonListener()); 
-		 VueTournee tournee = VueTournee.getInstance();
-			
-			// a l'ajout d'un point de livraison, on affiche les tranches horaires qui extistent dans la tournee.
-			if (Controleur.getInstance().getMode()==1){	
-				
-		
-				for(int index=0;index<tournee.getLesTrancheHoraires().length;index++){
-					comboBoxTrancheHoraire.addItem(tournee.getLesVuePointLivraisons().get(index).getPointLivraison().getTrancheHoraire());
-					comboBoxTrancheHoraire.setVisible(true);
-					comboBoxTrancheHoraire.addItemListener(new ItemState());
-					comboBoxTrancheHoraire.addActionListener(new ItemAction());
-					 bouton.addActionListener(new BoutonListener()); 
-							}
-			}
-			// pour supprimer un point de livraison, on affiche les point de livraison sur le noeud clique
-			else if(Controleur.getInstance().getMode()==2){
-				
-				for(int indexPointLivraison=0;indexPointLivraison<tournee.getLesVuePointLivraisons().size();indexPointLivraison++){
-					comboBoxPointLivraison.addItem(tournee.getLesVuePointLivraisons().get(indexPointLivraison).getPointLivraison());
-					comboBoxPointLivraison.setVisible(true);
-					comboBoxPointLivraison.addItemListener(new ItemState());
-					comboBoxPointLivraison.addActionListener(new ItemAction2());
-					 bouton.addActionListener(new BoutonListener2()); 
-				}
-			}
-				
-		
-		
-		
-		
-		 
 	}
-	 class ItemState implements ItemListener{
-		    public void itemStateChanged(ItemEvent e) {
-		    e.getItem();
-		    }               
-		  }
-	 class ItemAction implements ActionListener{
-		    public void actionPerformed(ActionEvent e) {
-		    	comboBoxTrancheHoraire.getSelectedItem().toString();
 	
-		    }               
-		  }
-	 class ItemAction2 implements ActionListener{
-		    public void actionPerformed(ActionEvent e) {
+	
+	public void ouvert(Noeud noeudClique){
+		noeud=noeudClique;
+		//TrancheHoraire trancheHoraire; 
+		
+		//creation de la fenetre
+		fenetre = new JFrame("Supervision");
+		fenetre.setSize(300,300);
+		//un clic sur la croix entraine la fermeture de la fenetre
+		//fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//centrer la fenetre par rapport a l'ecran de l'ordi
+		fenetre.setLocationRelativeTo(null);
+		
+		
+		fenetre.getContentPane().setLayout(null);
+		
+		boutonOK = new JButton("OK");
+		boutonOK.setLayout(null);
+		boutonOK.setBounds(120, 220, 100, 40);
+		fenetre.getContentPane().add(boutonOK, "South");
+		
+		boutonOK.addActionListener(new BoutonListener());
+		
+		Tournee tournee = Tournee.getInstance();
+		comboBox = new JComboBox<TrancheHoraire>();
+		for(int i=0; i<tournee.getTranchesHoraire().size();i++){
+			comboBox.addItem(tournee.getTranchesHoraire().get(i));
+		}
+		label = new JLabel("TranchesHoraires");
+		JPanel top = new JPanel();
+		//comboBox.setLayout(null);
+		comboBox.setBounds(0, 100, 200, 100);
+		label.setLayout(null);
+		label.setBounds(0, 0, 200, 100);
+	    top.add(label);
+	    top.add(comboBox);
+	   
+	    top.setLayout(null);
+	    top.setBounds(0, 0, 200, 200);
+	    fenetre.getContentPane().add(top, "North");		
+	    
+				
+		
+		 comboBox.addItemListener(new ItemState());
+		 comboBox.addActionListener(new ItemAction());
 		 
-		    	comboBoxPointLivraison.getSelectedItem().toString();
-		    }               
-		  }
-	 
-	 //Classe écoutant notre bouton
-	   public class BoutonListener implements ActionListener{
-	      public void actionPerformed(ActionEvent arg0) {
+		 fenetre.repaint();
+			
+		 fenetre.setVisible(true);
+	}
+	public void ouvert2(Noeud noeudClique){
+		noeud=noeudClique;
+		//TrancheHoraire trancheHoraire; 
+		
+		//creation de la fenetre
+		fenetre = new JFrame("Supervision");
+		fenetre.setSize(300,300);
+		//un clic sur la croix entraine la fermeture de la fenetre
+		//fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//centrer la fenetre par rapport a l'ecran de l'ordi
+		fenetre.setLocationRelativeTo(null);
+		
+		
+		fenetre.getContentPane().setLayout(null);
+		
+		boutonOK = new JButton("OK");
+		boutonOK.setLayout(null);
+		boutonOK.setBounds(120, 220, 100, 40);
+		fenetre.getContentPane().add(boutonOK, "South");
+		
+		boutonOK.addActionListener(new BoutonListener2());
+		
+		VueTournee tournee = VueTournee.getInstance();
+		comboBox2 = new JComboBox<PointLivraison>();
+		for(int i=0; i<tournee.getLesVuePointLivraisons().size();i++){
+			
+			comboBox2.addItem(tournee.lesPointLivraisonsClique(noeudClique.getX(), noeudClique.getY()).get(i));
+		}
+		label = new JLabel("PointLivraison");
+		JPanel top = new JPanel();
+		//comboBox.setLayout(null);
+		comboBox2.setBounds(0, 100, 200, 100);
+		label.setLayout(null);
+		label.setBounds(0, 0, 200, 100);
+	    top.add(label);
+	    top.add(comboBox2);
+	   
+	    top.setLayout(null);
+	    top.setBounds(0, 0, 200, 200);
+	    fenetre.getContentPane().add(top, "North");		
 	    
-	      Controleur.getInstance().ajoutPointLivraison(noeud, comboBoxTrancheHoraire.getItemAt(comboBoxTrancheHoraire.getSelectedIndex()));
-	       bouton.setEnabled(true);
-	    
-	     }
-	   }
-	   public class BoutonListener2 implements ActionListener{
-		      public void actionPerformed(ActionEvent arg0) {
-		    
-		      Controleur.getInstance().supprimerPointLivraison(comboBoxPointLivraison.getItemAt(comboBoxPointLivraison.getSelectedIndex()));
-		       bouton.setEnabled(true);
-		    
-		     }
-		   }
+				
+		
+	    comboBox2.addItemListener(new ItemState());
+	    comboBox2.addActionListener(new ItemAction2());
+		 
+		 fenetre.repaint();
+			
+		 fenetre.setVisible(true);
+	}
+	
+	 class ItemState implements ItemListener{
+		 public void itemStateChanged(ItemEvent e) {
+			 //e.getItem();
+			 System.out.println("evenement declenche sur : " + e.getItem());
+		  }               
+	}
+
+	 class ItemAction implements ActionListener{
+		 public void actionPerformed(ActionEvent e) {
+			 //comboBox.getSelectedItem();
+			 System.out.println("ActionListener : action sur " + comboBox.getSelectedItem());
+			 trancheHoraire = comboBox.getItemAt(comboBox.getSelectedIndex());
+		 }               
+	 }
+	 class ItemAction2 implements ActionListener{
+		 public void actionPerformed(ActionEvent e) {
+			 //comboBox.getSelectedItem();
+			 System.out.println("ActionListener : action sur " + comboBox2.getSelectedItem());
+			 pointLivraison = comboBox2.getItemAt(comboBox2.getSelectedIndex());
+		 }               
+	 }
+	 public class BoutonListener implements ActionListener{
+		 public void actionPerformed(ActionEvent e) {
+			 System.out.println(trancheHoraire.toString());
+			 Controleur.getInstance().ajoutPointLivraison(noeud, trancheHoraire);
+			 fenetre.dispose();
+			 //lancer controleur avec le noeud(comment passer l'arg?) et la tranche horaire(ok) puis fermer la fenetre
+
+		 }
+	}
+	 public class BoutonListener2 implements ActionListener{
+		 public void actionPerformed(ActionEvent e) {
+			 System.out.println(pointLivraison.toString());
+			 Controleur.getInstance().supprimerPointLivraison(pointLivraison);
+			 fenetre.dispose();
+			 //lancer controleur avec le noeud(comment passer l'arg?) et la tranche horaire(ok) puis fermer la fenetre
+
+		 }
+	}
 }
