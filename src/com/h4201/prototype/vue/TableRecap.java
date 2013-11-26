17119f47ed730b2405c8db3adf8c30abc7b71d14
Paul <paul.molins@gmail.com>
2013-11-26 20:10:09 +0100

    Modification affichage

--- src/com/h4201/prototype/vue/TableRecap.java
+++ src/com/h4201/prototype/vue/TableRecap.java
@@ -2,19 +2,26 @@ package com.h4201.prototype.vue;
 
 import java.util.Vector;
 
+import javax.swing.JTable;
+
 
 //import javax.swing.table.AbstractTableModel;
 
 
 import com.h4201.prototype.modele.PointLivraison;
+import com.h4201.prototype.modele.Tournee;
 import com.h4201.prototype.modele.TrancheHoraire;
 import com.h4201.prototype.utilitaire.Date;
 
 public class TableRecap {
 	
-	Vector <String> lesLivraisons;
+	private Vector <String> lesLivraisons;
+	private JTable tableau;
 
-	public TableRecap(Vector <TrancheHoraire> tranchesHoraires) {
+	public TableRecap()
+	{
+		Vector <TrancheHoraire> tranchesHoraires = Tournee.getInstance().getTranchesHoraire();
+		
 		lesLivraisons = new Vector<String>();
 		String listeLivraisonPourTH;
 		for(TrancheHoraire trancheHoraire : tranchesHoraires )
@@ -29,13 +36,37 @@ public class TableRecap {
 			
 		    for(PointLivraison pointLivraison : trancheHoraire.getPointsLivraisons())
 		    {
-		    	listeLivraisonPourTH = "\t\tL" + pointLivraison.getIdPointLivraison()
-		    			+ " a " + Date.getHeureFrSimplifieeDepuisCalendar(pointLivraison.getHeureArriveeEstimee());
+		    	listeLivraisonPourTH = "\t\tL" + pointLivraison.getIdPointLivraison();
+		    			
+		    	if(pointLivraison.getHeureArriveeEstimee() != null)
+		    		listeLivraisonPourTH += " a " + Date.getHeureFrSimplifieeDepuisCalendar(
+		    				pointLivraison.getHeureArriveeEstimee());
+		    	
 		    	lesLivraisons.add(listeLivraisonPourTH);
 		    }
 		}
+		
+		// Transformation structure de donnees
+		Object[][] objets = new Object[this.getLongueur()][1];
+		String[] entetes = {"Livraisons par tranche horaire"};
+		for(int compte=0; compte<this.getLongueur(); compte++){
+			objets[compte][0]=this.getLesLivraisons().get(compte);
+		}
+		
+		tableau = new JTable(objets, entetes) {
+			private static final long serialVersionUID = -1070527324413234766L;
+
+			@Override
+			public boolean isCellEditable(int row, int col) {
+				return false;
+			}
+		};
 	}
 	
+	public JTable getTableau()
+	{
+		return tableau;
+	}
 	
 	public Vector<String> getLesLivraisons(){
 		return lesLivraisons;
