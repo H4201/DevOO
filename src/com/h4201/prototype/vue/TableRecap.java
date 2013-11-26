b95d601d99db27b1ce2ef8cab4340a76e0e06235
Paul <paul.molins@gmail.com>
2013-11-26 17:09:51 +0100

    Amélioration affichage

--- src/com/h4201/prototype/vue/TableRecap.java
+++ src/com/h4201/prototype/vue/TableRecap.java
@@ -8,6 +8,7 @@ import java.util.Vector;
 
 import com.h4201.prototype.modele.PointLivraison;
 import com.h4201.prototype.modele.TrancheHoraire;
+import com.h4201.prototype.utilitaire.Date;
 
 public class TableRecap {
 	
@@ -18,12 +19,18 @@ public class TableRecap {
 		String listeLivraisonPourTH;
 		for(TrancheHoraire trancheHoraire : tranchesHoraires )
 		{
-			listeLivraisonPourTH = trancheHoraire.toString() + "\n";
+			listeLivraisonPourTH = "TH" + trancheHoraire.getIdTrancheHoraire() 
+					+ " entre " 
+					+ Date.getHeureFrSimplifieeDepuisCalendar(trancheHoraire.getHeureDebut()) 
+					+ " et " 
+					+ Date.getHeureFrSimplifieeDepuisCalendar(trancheHoraire.getHeureFin());
+			
 			lesLivraisons.add(listeLivraisonPourTH);
 			
 		    for(PointLivraison pointLivraison : trancheHoraire.getPointsLivraisons())
 		    {
-		    	listeLivraisonPourTH = "\t" + pointLivraison.toString() + "\n";
+		    	listeLivraisonPourTH = "\t\tL" + pointLivraison.getIdPointLivraison()
+		    			+ " a " + Date.getHeureFrSimplifieeDepuisCalendar(pointLivraison.getHeureArriveeEstimee());
 		    	lesLivraisons.add(listeLivraisonPourTH);
 		    }
 		}
